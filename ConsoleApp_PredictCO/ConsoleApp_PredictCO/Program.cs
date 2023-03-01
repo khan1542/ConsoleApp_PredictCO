00415215fb6927c706393560e4347f5b2829563b
Константин <54914190+khan1542@users.noreply.github.com>
2023-03-01 13:55:41 +0500

    Added MLModel for Predicting CO Value
    Console APP and MLModel added

--- ConsoleApp_PredictCO/ConsoleApp_PredictCO/Program.cs
+++ ConsoleApp_PredictCO/ConsoleApp_PredictCO/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp_PredictCO;
+
+Console.WriteLine("Прогнозирование Степени использования CO,%");
+Console.WriteLine("Введите ID параметра");
+string parameterid = Console.ReadLine();
+Console.WriteLine("Введите дату в формате ДД.ММ.ГГГГ");
+string data = Console.ReadLine();
+
+//Load sample data
+MLModel_PredictCO.ModelInput sampleData = new MLModel_PredictCO.ModelInput()
+{
+    ParameterId = Convert.ToInt32(parameterid),
+    Date = data,
+};
+
+// Make a single prediction on the sample data and print results
+var predictionResult = MLModel_PredictCO.Predict(sampleData);
+
+Console.WriteLine($"\n\nПрогнозируемая Степень использования CO,%: {predictionResult.Score}\n\n");
+Console.WriteLine("=============== End of process, hit any key to finish ===============");
+Console.ReadKey();
+
